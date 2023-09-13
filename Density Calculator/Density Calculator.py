68d433bff53602d240b7ebcf98e847d351488506
GenuinelyAref <aref.osman18@gmail.com>
2023-09-14 00:45:02 +1200

    v2 (final, working program)

--- Density Calculator/Density Calculator.py
+++ Density Calculator/Density Calculator.py
@@ -1,15 +1,59 @@
 # tool to help in calculating density of object
 
+# find volume of unit cell based on atomic radius and structure type
 def find_volume(var_structure_type, var_radius):
-    var_radius = var_radius / (10 ** 9) # convert to metres
+    var_radius = var_radius / (10 ** 9)  # convert to metres
     if var_structure_type == 'bcc':
         a = (4 * var_radius) / (3 ** 0.5)
     else:
         a = (4 * var_radius) / (2 ** 0.5)
-    var_volume = a^3 * (10 ** 6)
+    var_volume = (a ** 3) * (10 ** 6)
     return var_volume
 
 
-# number of atoms in unit cell
+# match number of atoms per unit cell with structure type
+def get_atom_num(var_structure_type):
+    if var_structure_type == 'bcc':
+        var_atom_num = 2
+    else:
+        var_atom_num = 4
+    return var_atom_num
+
+
+# calculate density
+def find_density(var_n, var_atomic_mass, var_volume, var_avogadro_num):
+    var_density = (var_n * var_atomic_mass) / (var_volume * var_avogadro_num)
+    return var_density
+
+
+# calculate the atomic radius
+def find_atomic_radius(var_n, var_atomic_mass, var_avogadro_num, var_density, var_structure_type):
+    var_atomic_volume = (var_n * var_atomic_mass) / (var_density * var_avogadro_num)
+    var_atomic_volume = var_atomic_volume / (10 ** 6)
+    if var_structure_type == 'bcc':
+        # calculate atomic radius for bcc and convert to nanometers
+        var_atomic_radius = ((var_atomic_volume ** (1/3)) * (3 ** 0.5) * 0.25) * (10 ** 9)
+    else:
+        # calculate atomic radius for fcc and convert to nanometers
+        var_atomic_radius = ((var_atomic_volume ** (1 / 3)) * (2 ** 0.5) * 0.25) * (10 ** 9)
+    return var_atomic_radius
+
+
 structure_type = 'bcc'  # could also be 'fcc'
-atomic_radius = 0.109 # in nanometers
+atomic_radius = 0.124  # in nanometers
+atomic_mass = 55.85  # in g mol^-1
+avogadro_num = 6.022 * (10 ** 23)  # in mol^-1
+rounding_dp = 5  # 3dp of rounding
+
+# calculate volume
+volume = find_volume(structure_type, atomic_radius)
+# calculate number of atoms in unit cell
+n = get_atom_num(structure_type)
+
+# calculate density
+density = find_density(n, atomic_mass, volume, avogadro_num)
+# calculate atomic radius
+atomic_radius_test = find_atomic_radius(n, atomic_mass, avogadro_num, density, structure_type)
+
+print("Density: {} g/cm\u00b3".format(round(density, rounding_dp)))
+print("Atomic radius = {} nm".format(round(atomic_radius_test, rounding_dp)))
