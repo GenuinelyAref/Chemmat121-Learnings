ffcb6b4c320af27b8ce68b3454b502f373114fa0
GenuinelyAref <aref.osman18@gmail.com>
2023-09-19 23:58:26 +1200

    add temperature calculation option

--- Arrhenius Equation/Arrhenius-Equation Calculator.py
+++ Arrhenius Equation/Arrhenius-Equation Calculator.py
@@ -11,3 +11,8 @@ def celsius_to_kelvin(var_celsius):
 def calc_rate(var_constant, var_activation_energy, var_r, var_temperature):
     rate = var_constant * (math.e ** ((-1 * var_activation_energy) / (var_r * var_temperature)))
     return rate
+
+
+def calc_temp(var_constant, var_activation_energy, var_r, var_rate):
+    var_temperature = (-1*(var_r/var_activation_energy))/(math.log(var_constant) - math.log(var_rate))
+    return var_temperature
