37e8710b58005e7dfe267785a1c5bc78e9b18a96
GenuinelyAref <aref.osman18@gmail.com>
2023-09-19 23:59:42 +1200

    testing

--- Arrhenius Equation/Arrhenius-Equation Calculator.py
+++ Arrhenius Equation/Arrhenius-Equation Calculator.py
@@ -16,3 +16,7 @@ def calc_rate(var_constant, var_activation_energy, var_r, var_temperature):
 def calc_temp(var_constant, var_activation_energy, var_r, var_rate):
     var_temperature = (-1*(var_r/var_activation_energy))/(math.log(var_constant) - math.log(var_rate))
     return var_temperature
+
+
+universal_gas_constant = 8.314  # J / mol K
+print(calc_rate(3*10^-6, 40000, universal_gas_constant, celsius_to_kelvin(85)))
