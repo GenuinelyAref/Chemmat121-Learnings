1452de4b97ebd5bad635636f2decef00f0ecad4f
GenuinelyAref <aref.osman18@gmail.com>
2023-10-04 23:59:53 +1300

    implement SI units & use experimental values
    - test basic working functionality

--- Fracture Mechanics Equation/Fracture Mechanics Equation.py
+++ Fracture Mechanics Equation/Fracture Mechanics Equation.py
@@ -12,10 +12,10 @@ def k_1_calculation(var_y, var_stress, var_a):
 # calculate a value, depending on crack size and crack type
 def a_type_calculation(var_crack_length, var_crack_type):
     if var_crack_type == "internal":
-        a = var_crack_length / 2
+        var_a = var_crack_length / 2
     else:
-        a = var_crack_length
-    return a
+        var_a = var_crack_length
+    return var_a
 
 
 # calculate dimensionless geometric factor
@@ -34,3 +34,25 @@ def stress_calculation(var_y, var_a, var_k_1):
 def a_value_calculation(var_y, var_stress, var_k_1):
     var_a = ((var_k_1 / (var_y * var_stress)) ** 2) / math.pi
     return var_a
+
+
+# converts any value to its SI unit size
+def convert_si_units(var_value, var_order_magnitude):
+    var_si_equivalent = var_value * (10 ** var_order_magnitude)
+    return var_si_equivalent
+
+
+def convert_conventional
+
+
+# MAIN ROUTINE
+a = 12  # in mm
+nominal_stress = 38  # in MPa
+y = 0.8  # dimensionless geometric constant
+
+# convert all units to SI
+a = convert_si_units(a, -3)
+nominal_stress = convert_si_units(nominal_stress, 6)
+
+k_1_value = k_1_calculation(y, nominal_stress, a)
+print("K_1 value is: {}".format(k_1_value))
