4e85b08f222bb77c36c4d8564c67bcda7860368b
GenuinelyAref <aref.osman18@gmail.com>
2023-10-28 23:54:02 +1300

    minor edits

--- Faraday Equation/Faraday Equation Calculator.py
+++ Faraday Equation/Faraday Equation Calculator.py
@@ -13,18 +13,23 @@ def get_current():
 
 
 def get_time():
-    time_unit = (input("Unit of time, choose from Y (year), W (week), D (day) or H (hour): ").lower())[0]
-    time_multiplier = 1
-    if time_unit == "Y":
+    time_unit = (input("Unit of time, choose from Y (year), W (week), D (day) or S (seconds): ").lower())[0]
+    time_prefixes = ["second", "day", "week", "year"]
+    if time_unit == "y":
         # unit is year
         time_multiplier = 31536000  # number of seconds in a year (based on 365 days)
-    elif time_unit == "W":
+    elif time_unit == "w":
         # unit is week
         time_multiplier = 604800  # number of seconds in a week
-    elif time_unit == "D":
+    elif time_unit == "d":
         # unit is day
         time_multiplier = 86400  # number of seconds in a day
     else:
-        # unit is hour
-        time_multiplier = 3600  # number of seconds in an hour
+        # unit is seconds
+        time_multiplier = 1  # number of seconds in an hour
+    try:
+        time_prefix = time_prefixes.index(time_unit)
+    except ValueError:
+
+    amount_time = input("Number of {}s: ")
 
