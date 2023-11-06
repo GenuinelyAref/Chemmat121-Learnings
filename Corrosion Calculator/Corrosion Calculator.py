5b4c23cceecf58267be9a90b63105e52e9128cfa
GenuinelyAref <aref.osman18@gmail.com>
2023-11-07 00:04:05 +1300

    create pseudocode for code structure

--- Corrosion Calculator/Corrosion Calculator.py
+++ Corrosion Calculator/Corrosion Calculator.py
@@ -1,9 +1,15 @@
 # program to calculate the different parameters of Faraday's equation
+# import libraries
 
 
 # functions
+# tkinter window for calculator mode
+
+# tkinter window for chosen value to calculate
+
 # tkinter windows for value inputs
 
-# tkinter window for rounding input
+# tkinter window for rounding accuracy input
 
 
+# Main routine
